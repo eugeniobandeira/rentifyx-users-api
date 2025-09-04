fb99ce6213a2a8350d1aeab91614f09e7b0bdf79
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-09-04 12:53:43 -0300

    fix: rename slices directories

---
+++ src/05 - IoC/Rentifyx-Clients.IoC/DependencyInjectionApi.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace RentifyxClients.IoC;
+public static class DependencyInjectionApi
+{
+    //public static IHostApplicationBuilder UseOpenApiTemplate(this IHostApplicationBuilder builder)
+    //{
+    //    builder.Services.AddOpenApi(options =>
+    //    {
+    //        options.AddDocumentTransformer((document, context, cancellationToken) =>
+    //        {
+    //            document.Info.Title = "Rentifyx API";
+                
+    //            return Task.CompletedTask;
+    //        });
+    //    });
+
+    //    return builder;
+    //}
+
+    public static WebApplication UseSwaggerUiTemplate(this WebApplication app)
+    {
+        app.MapOpenApi();
+        
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/openapi/v1.json", "Rentifyx");
+        });
+
+        return app;
+    }
+
+}
