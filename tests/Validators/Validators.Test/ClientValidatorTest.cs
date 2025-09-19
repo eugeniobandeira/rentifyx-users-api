15008336c4dbb80bc8a8ed1e129306cc5a89fc25
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-09-19 17:46:29 -0300

    test: client validator test

---
+++ tests/Validators/Validators.Test/ClientValidatorTest.cs
@@ -0,0 +1,29 @@
+﻿using CommomTestUtilities.Request;
+using Rentifyx.Clients.Application.Features.Clients.Handler.Create;
+using Rentifyx.Clients.Application.Features.Clients.Request;
+
+namespace Validators.Test;
+
+public class ClientValidatorTest
+{
+    [Fact]
+    public void ShouldBeValidWhenClientIsValid()
+    {
+        // Arrange
+        var client = ClientBuilder.Build();
+
+        var request = new CreateClientRequest(
+            client.Document,
+            client.Name,
+            client.Email);
+
+        var validator = new CreateClientValidator();
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        Assert.True(result.IsValid);
+        Assert.Empty(result.Errors);
+    }
+}
