66cba6ee1f2d9e5f879366c115ac9b5500751260
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-09-19 17:45:34 -0300

    test: client builder

---
+++ tests/CommomTestUtilities/CommomTestUtilities/Request/ClientBuilder.cs
@@ -0,0 +1,32 @@
+﻿using Bogus;
+using Bogus.Extensions.Brazil;
+using Rentifyx.Clients.Domain.Entities;
+using System;
+using System.Security.Cryptography;
+
+namespace CommomTestUtilities.Request;
+
+#pragma warning disable CA1515 // Consider making public types internal
+public static class ClientBuilder
+#pragma warning restore CA1515 // Consider making public types internal
+{
+    public static ClientEntity Build()
+    {
+        return new Faker<ClientEntity>("pt_BR")
+            .RuleFor(c => c.Document, f =>
+            {
+                var randomNumber = RandomNumberGenerator.GetInt32(0, 2);
+                var document = randomNumber == 0
+                    ? f.Person.Cpf()
+                    : f.Company.Cnpj();
+
+                return document
+                    .Replace(".", "", StringComparison.Ordinal)
+                    .Replace("-", "", StringComparison.Ordinal)
+                    .Replace("/", "", StringComparison.Ordinal);
+            })
+            .RuleFor(c => c.Name, f => f.Name.FullName())
+            .RuleFor(c => c.Email, f => f.Internet.Email())
+            .Generate();
+    }
+}
