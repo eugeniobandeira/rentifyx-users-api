bf8de7e043e0a204dea3f0fa75da134a50ffd2f7
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-09-19 17:44:14 -0300

    feat: create client request

---
+++ src/02 - Application/Rentifyx-Clients.Application/Features/Clients/Request/CreateClientRequest.cs
@@ -0,0 +1,8 @@
+﻿using System.Globalization;
+
+namespace Rentifyx.Clients.Application.Features.Clients.Request;
+
+public record CreateClientRequest(
+    string Document,
+    string Name, 
+    string Email);
