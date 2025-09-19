ef682ce0ea2313c5047de16ec2c67b981171e0c1
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-09-19 17:43:23 -0300

    feat: client adapter

---
+++ src/02 - Application/Rentifyx-Clients.Application/Adapter/ClientAdapter.cs
@@ -0,0 +1,19 @@
+﻿using Rentifyx.Clients.Application.Features.Clients.Request;
+using Rentifyx.Clients.Domain.Entities;
+
+namespace Rentifyx.Clients.Application.Adapter;
+
+public static class ClientAdapter
+{
+    public static ClientEntity FromRequestToEntity(CreateClientRequest request)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+
+        return new ClientEntity()
+        {
+            Name = request.Name,
+            Document = request.Document,
+            Email = request.Email
+        };
+    }
+}
