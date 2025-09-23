a853402b99452e285a05f7284bb4259b4abf0188
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-09-22 21:38:37 -0300

    test: create client validator

--- tests/Validators/Validators.Test/ClientValidatorTest.cs
+++ tests/Validators/Validators.Test/ClientValidatorTest.cs
@@ -1,6 +1,8 @@
 ﻿using CommomTestUtilities.Request;
+using FluentAssertions;
 using Rentifyx.Clients.Application.Features.Clients.Handler.Create;
 using Rentifyx.Clients.Application.Features.Clients.Request;
+using Rentifyx.Clients.Domain.MessageResource;
 
 namespace Validators.Test;
 
@@ -26,4 +28,176 @@ public class ClientValidatorTest
         Assert.True(result.IsValid);
         Assert.Empty(result.Errors);
     }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("      ")]
+    public void ShouldBeInvalidWhenClientDocumentIsNull(string document)
+    {
+        // Arrange
+        var client = ClientBuilder.Build();
+
+        var request = new CreateClientRequest(
+            document,
+            client.Name,
+            client.Email);
+
+        var validator = new CreateClientValidator();
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+
+        var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+        var totalErrors = 2;
+
+        errorMessages.Should().Contain(ClientValidatorMessageResource.EMPTY_DOCUMENT);
+        errorMessages.Should().Contain(ClientValidatorMessageResource.DOCUMENT_LENGTH);
+
+        result.Errors.Should().HaveCount(totalErrors);
+    }
+
+    [Theory]
+    [InlineData("1234567891")]
+    [InlineData("12345678912345678")]
+    [InlineData("123")]
+    public void ShouldBeInvalidWhenClientDocumentHasInvalidLength(string document)
+    {
+        // Arrange
+        var client = ClientBuilder.Build();
+
+        var request = new CreateClientRequest(
+            document,
+            client.Name,
+            client.Email);
+
+        var validator = new CreateClientValidator();
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+
+        result.Errors
+            .Should()
+            .ContainSingle()
+            .And
+            .Contain(error => error.ErrorMessage == ClientValidatorMessageResource.DOCUMENT_LENGTH);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("      ")]
+    public void ShouldBeInvalidWhenClientNameIsNull(string name)
+    {
+        // Arrange
+        var client = ClientBuilder.Build();
+
+        var request = new CreateClientRequest(
+            client.Document,
+            name,
+            client.Email);
+
+        var validator = new CreateClientValidator();
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+
+        var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+        var totalErrors = 2;
+
+        errorMessages.Should().Contain(ClientValidatorMessageResource.EMPTY_NAME);
+        errorMessages.Should().Contain(ClientValidatorMessageResource.NAME_MIN_LENGTH);
+
+        result.Errors.Should().HaveCount(totalErrors);
+    }
+
+    [Fact]
+    public void ShouldBeInvalidWhenClientNameIsMoreThan100Digits()
+    {
+        // Arrange
+        var client = ClientBuilder.Build();
+
+        var request = new CreateClientRequest(
+            client.Document,
+            "fake name fake name fake name fake name fake name fake name fake name fake name fake name fake name fake name fake name fake name",
+            client.Email);
+
+        var validator = new CreateClientValidator();
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+
+        result.Errors
+            .Should()
+            .ContainSingle()
+            .And
+            .Contain(error => error.ErrorMessage == ClientValidatorMessageResource.NAME_MAX_LENGTH);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("      ")]
+    public void ShouldBeInvalidWhenClientEmailIsNull(string email)
+    {
+        // Arrange
+        var client = ClientBuilder.Build();
+
+        var request = new CreateClientRequest(
+            client.Document,
+            client.Name,
+            email);
+
+        var validator = new CreateClientValidator();
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+
+        var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+        var totalErrors = 2;
+
+        errorMessages.Should().Contain(ClientValidatorMessageResource.EMPTY_EMAIL);
+        errorMessages.Should().Contain(ClientValidatorMessageResource.INVALID_EMAIL_ADDRESS);
+
+        result.Errors.Should().HaveCount(totalErrors);
+    }
+
+    [Fact]
+    public void ShouldBeInvalidWhenClientEmailIsNotValid()
+    {
+        // Arrange
+        var client = ClientBuilder.Build();
+
+        var request = new CreateClientRequest(
+            client.Document,
+            client.Name,
+            "test.com.br");
+
+        var validator = new CreateClientValidator();
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+
+        result.Errors
+            .Should()
+            .ContainSingle()
+            .And
+            .Contain(error => error.ErrorMessage == ClientValidatorMessageResource.INVALID_EMAIL_ADDRESS);
+    }
+
 }
