0ba35a5c374c55e08c047040d1caf6bc0ff0c5ff
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-09-23 22:37:27 -0300

    feat: client repository

---
+++ src/03 - Domain/Rentifyx-Clients.Domain/Interfaces/Client/IClientWriteOnlyRepository.cs
@@ -0,0 +1,8 @@
+﻿using Rentifyx.Clients.Domain.Entities;
+
+namespace Rentifyx.Clients.Domain.Interfaces.Client;
+
+public interface IClientWriteOnlyRepository
+{
+    Task AddAsync(ClientEntity clientEntity, CancellationToken cancellationToken = default);
+}
---
+++ src/06 - Infrastructure/Rentifyx-Clients.Infrastructure/Repositories/ClientRepository.cs
@@ -0,0 +1,12 @@
+﻿using Rentifyx.Clients.Domain.Entities;
+using Rentifyx.Clients.Domain.Interfaces.Client;
+
+namespace Rentifyx.Clients.Infrastructure.Repositories;
+
+internal class ClientRepository : IClientWriteOnlyRepository
+{
+    public Task AddAsync(ClientEntity clientEntity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
