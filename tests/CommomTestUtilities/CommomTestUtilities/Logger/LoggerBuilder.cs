09edd80a0ae49b03488a3f0d85d10cfa218c3854
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-09-23 22:38:51 -0300

    test: logger builder

---
+++ tests/CommomTestUtilities/CommomTestUtilities/Logger/LoggerBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace CommomTestUtilities.Logger;
+
+public static class LoggerBuilder<T> where T : class
+{
+    public static ILogger<T> Build()
+    {
+        var logger = new Mock<ILogger<T>>();
+
+        return logger.Object;
+    }
+}
