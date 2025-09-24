be747b227a9cbe47f9c72ad4569f155944aeb99a
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-09-23 22:38:21 -0300

    feat: exception validation

---
+++ src/04 - Exception/Rentifyx-Clients.Exception/ExceptionBase/ErrorOnValidationException.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using System.Net;
+
+namespace Rentifyx.Clients.Exceptions.ExceptionBase;
+
+public class ErrorOnValidationException : ClientsException
+{
+    private readonly ReadOnlyCollection<string> _errors;
+
+    public ErrorOnValidationException(ReadOnlyCollection<string> errorMessages)
+        : base(string.Empty)
+    {
+        _errors = errorMessages;
+    }
+
+    public ErrorOnValidationException()
+        : base(string.Empty)
+    {
+        _errors = new ReadOnlyCollection<string>(Array.Empty<string>());
+    }
+
+    public ErrorOnValidationException(string message) 
+        : base(message)
+    {
+        _errors = new ReadOnlyCollection<string>(Array.Empty<string>());
+    }
+
+    public ErrorOnValidationException(string message, Exception innerException) 
+        : base(message, innerException)
+    {
+        _errors = new ReadOnlyCollection<string>(Array.Empty<string>());
+    }
+
+    public override int StatusCode 
+        => (int)HttpStatusCode.BadRequest;
+
+    public override ReadOnlyCollection<string> GetErrors()
+    {
+        return _errors;
+    }
+}
