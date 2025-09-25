b3f8117de1f37c318f719b2d2600a97d71a3ee4f
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-09-25 13:53:59 -0300

    feat(IoC): dependency injection

---
+++ src/05 - IoC/Rentifyx.Clients.IoC/ApplicationDependencyInjectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Rentifyx.Clients.Application.Features.Clients.Handler.Create;
+
+namespace Rentifyx.Clients.IoC;
+
+public static class ApplicationDependencyInjectionExtension
+{
+    public static void AddApplicationDependencies(this IServiceCollection services)
+    {
+        AddHandlers(services);
+    }
+
+    private static void AddHandlers(IServiceCollection services)
+    {
+        services.AddScoped<ICreateClientHandler, CreateClientHandler>();
+    }   
+}
---
+++ src/05 - IoC/Rentifyx.Clients.IoC/InfrastructureDependencyInjectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.DynamoDBv2;
+using Microsoft.Extensions.DependencyInjection;
+using Rentifyx.Clients.Domain.Interfaces.Client;
+using Rentifyx.Clients.Infrastructure.Repositories;
+
+namespace Rentifyx.Clients.IoC;
+
+public static class InfrastructureDependencyInjectionExtension
+{
+    public static void AddInfrastructureDependencies(this IServiceCollection services)
+    {
+        AddRepositories(services);
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
+        services.AddAWSService<IAmazonDynamoDB>();
+        services.AddScoped<IClientWriteOnlyRepository, ClientRepository>();
+    }   
+}
