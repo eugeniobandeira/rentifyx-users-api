50cb675f9f1b6f343efbe851589b6494facb7899
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-09-25 13:52:57 -0300

    feat: client repository implementation

---
+++ src/06 - Infrastructure/Rentifyx.Clients.Infrastructure/Repositories/ClientRepository.cs
@@ -0,0 +1,51 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Rentifyx.Clients.Domain.Entities;
+using Rentifyx.Clients.Domain.Interfaces.Client;
+using System.Net;
+
+namespace Rentifyx.Clients.Infrastructure.Repositories;
+
+public sealed class ClientRepository : IClientWriteOnlyRepository
+{
+    private readonly IAmazonDynamoDB _dynamoDb;
+    private readonly string _tableName;
+
+    public ClientRepository(
+        IAmazonDynamoDB amazonDynamoDB,
+        string tableName = "rentifyx-clients-dev")
+    {
+        _dynamoDb = amazonDynamoDB;
+        _tableName = tableName;
+    }
+
+    public async Task<bool> PutItemAsync(ClientEntity clientEntity, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(clientEntity);
+
+        try
+        {
+            var item = new Dictionary<string, AttributeValue>
+            {
+                { "Document", new AttributeValue { S = clientEntity.Document } },
+                { "Name", new AttributeValue { S = clientEntity.Name } },
+                { "EmailAddress", new AttributeValue { S = clientEntity.Email } }
+            };
+
+            var request = new PutItemRequest()
+            {
+                TableName = _tableName,
+                Item = item
+            };
+
+            var response = await _dynamoDb.PutItemAsync(request, cancellationToken);
+
+            return response.HttpStatusCode == HttpStatusCode.OK;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception {ex.Message}");
+            throw;
+        }
+    }
+}
