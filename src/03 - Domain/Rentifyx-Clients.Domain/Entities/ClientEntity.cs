0de0192cd9ebef9892e4b6c478c04c4b77174157
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-09-25 13:50:56 -0300

    feat: client entity

--- src/03 - Domain/Rentifyx-Clients.Domain/Entities/ClientEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Rentifyx.Clients.Domain.Entities;
-
-public sealed class ClientEntity
-{
-    public string Document { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-}
